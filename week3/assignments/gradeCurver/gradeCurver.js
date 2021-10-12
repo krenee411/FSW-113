bb3d1450c9856526510d3cd68f250b1fc4ad3e6b
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-10-11 21:40:16 -0500

    grades added

--- week3/assignments/gradeCurver/gradeCurver.js
+++ week3/assignments/gradeCurver/gradeCurver.js
@@ -3,6 +3,17 @@
 // create an event listener that calls the curveGrade() function when the Curve It!! button is clicked
 
 // create an event listener that resets the scores and grades to their defaults when the Reset button is clicked
+let aryGrades, gradeSlice, mean, range
+
+var submit = document.getElementById('submit').addEventListener('click', function(){
+    curveGrades()
+})
+var grades = document.getElementById('grades')
+grades.innerHTML += submit
+var reset = document.getElementById('reset').addEventListener('click', function(){
+    document.getElementById('scores').value = ""
+    document.getElementById('grades').innerText = ""
+})
 
 function applyBell(grade, index, ary) {
     switch (true) {
@@ -32,37 +43,32 @@ function convertArray(obj) {
     return ary
 }
 
+
 // Condense the number of lines within the curveGrade() function as much as possible by converting 
 // the functions to arrow functions. You can also condense the number of lines by combining some 
 // separate lines of code into single lines. It currently has 18 lines of code. Without counting  
 // empty lines, can you get the number of lines down to 8?
 
 function curveGrades() {
-    **sum = function (accumulator, currentValue) {
-        return accumulator + currentValue
-    }
-
-    **sumGrades = function(array) {
-        return array.reduce(sum)
-    }
-
-    **aryGrades = convertArray(document.querySelector('#scores'))
-
-    **minGrade = aryGrades.reduce(function(a, b) {
-        return Math.min(a, b)
-    })
     
-    **maxGrade = aryGrades.reduce(function(a, b) {
-        return Math.max(a, b)
-    })
+    let sum = ((accumulator, currentValue) =>  accumulator + currentValue)
     
-    **mean = sumGrades(aryGrades) / aryGrades.length
+    let sumGrades = (array => array.reduce(sum))
+    
+    let aryGrades = convertArray(document.querySelector('#scores'))
 
-    **range = maxGrade - minGrade
+    let minGrade = aryGrades.reduce( (a, b) => Math.min(a, b))
+    
+    let maxGrade = aryGrades.reduce((a, b) => Math.max(a, b))
+    
+     mean = sumGrades(aryGrades) / aryGrades.length // im not sure whybut wheni labe the variable with let it does not read the mean??
+
+    let range = maxGrade - minGrade
 
     gradeSlice = range / 5
 
     aryGrades.forEach(applyBell)
 
     // write the value of aryGrades to the grades div in the HTML document
+    document.getElementById('grades').innerText = aryGrades
 }
