d3af4f58c257a45c065c2698455fb70aec44efc0
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-10-23 19:18:20 -0500

    assigments added

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -3,30 +3,32 @@ const lang = 'JavaScript'
 // Create an event listener for the submit button that adds all 'input' elements to 
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
-let inputs = document.getElementsByTagName("input") 
-//-console.log(inputs)
+document.getElementById('submit').addEventListener('click',chkLang)
 
-function chkLang(lang,...value) {
-    result = false
-    //console.log(value)
-    let obj = document.querySelector('#TestResult')
-for (i=0; i<value.length; i++){
-    //console.log(obj,value[i],lang) 
-    if(value[i]=== lang){result = true}
-}
-if(result){
-    obj.innerHTML = `Congratulations!\nYou know ${lang}.`
-}else{
-    obj.innerHTML = `Sorry,\nyou don't know ${lang}.`
-}
-}
-
-
+function chkLang(langs) {
+    let result = false
+    var inputs = document.getElementsByTagName('input')
+    //console.log(inputs)
+    var items= []
+    for(i= 0; i<inputs.length; i++){
+       var area = inputs[i].value.split(',') 
+       items= [...items,...area]
+     
+    }
+      //  console.log(items)
+     items.find(function(getIt){
+     if(items === `${lang}`){
+         return result  === false
+     }
+    })
+    
+ 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
-
-    //let obj = document.querySelector('#TestResult')
-    //if (result) 
-    //    obj.innerText = `Congratulations!\nYou know ${lang}.`
-    //else
-       // obj.innerText = `Sorry,\nyou don't know ${lang}.`'
+    let obj = document.querySelector('#TestResult')
+    if (result) 
+        obj.innerText = `Congratulations!\nYou know ${lang}.`
+    else
+        obj.innerText = `Sorry,\nyou don't know ${lang}.`
+ 
+}
