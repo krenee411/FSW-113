17ae463b821b13703697a3fbc23ddca4ffff1efe
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-11-02 21:53:58 -0500

    week 5 update

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,34 +1,48 @@
 // Declare any necessary variables.
-var value= document.getElementsByTagName("text")
+
 // Add an event listener that responds to the click of the "print" button by calling a function to instantiate
-var submit = document.getElementById('print').addEventListener('click', function(){
-    let myStudent= new Students()
-    //certStudentName(myStudent)
-})
+
+document.querySelector('#print').addEventListener('click',() => certStudent(newStudent()))
+
 //  a new student object, and another function to print the certificate.
 
+function newStudent(){
+let studentName = document.querySelector('#studentName').value;
+let course = document.querySelector('#className').value;
+let scores = convert(document.querySelector('#studentScores').value);
+let possScore = convert(document.querySelector('#possibleScores').value);
+   return new Students(studentName,course,scores, possScore)
+}
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
-var reset = document.getElementById('reset').addEventListener('click', function() {
-    return reset.value = " "
+document.querySelector("#reset").addEventListener("click", reset())
 
-})
+function reset(){
+  var clearInfo = [...document.querySelectorAll("input")]
+  clearInfo.forEach(function(e, i){
+    clearInfo[i].value = ""
+  })
+  var clearCert = [...document.querySelectorAll(".cert-data")]
+    clearCert.forEach(function(e, i){
+      clearCert[i].innerHTML = ""
+    })
+  }
+
 
 // Create a function that instantiates a new student object with the input from the HTML form.
-function instantiateStudent() {
-
+function certStudent(studentInfo) {
+  document.querySelector("#certStudentName").innerHTML = studentInfo.getName()
+  document.querySelector('#certClassName').innerHTML = studentInfo.getCourse()
+  document.querySelector('#certGrade').textContent = studentInfo.letterGrade()
+  console.log(studentInfo)
 }
+
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
-function printCertificate() {
-    let myStudent= new Students()
-    certStudentName(myStudent)
-}
 
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
-function certStudent(){
-    document.querySelector("#certStudentName").innerHTML = studentName
-    document.querySelector("#certClassName").innerHTML = className
-    document.querySelector("#certGrade").innerHTML = scores   
+
+function convert(array){
+ return array.split(",").map(e => parseInt(e))
 }
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -1,50 +1,39 @@
 // Create a class called Student whose constructor maintains all four data inputs from the HTML page.
 class Students{
-    constructor(){
-        this.studentName= document.querySelector("#studentName").value;
-        this.className= document.querySelector("#className").value;
-        this.scores= document.querySelector("#studentScores").value;
-        this.possible= document.querySelector("#possibleScores").value;
-        console.log(this.studentName, this.className, this.scores, this.possible)
+    constructor(name, course, scores, possible){
+        this.name= name;
+        this.course= course
+        this.scores= scores;
+        this.possible= possible;
+       // console.log(this.studentName, this.className, this.scores, this.possible)
     }
 
 // The class should also contain the following methods:
 // - a method that adds up all the student's scores
-addScores(){
-    currentGrade = this.scores.value
-    for (i=0; i<currentGrade.length; i++){
-        currentGrade[i] + 0
+getName(){
+    return this.name
+}
+getCourse(){
+    return this.course
+}
 
-    }
-    console.log(addScores())
+
+addScores(){
+    return this.scores.reduce((possGrade, currentGrade) => (possGrade + currentGrade))
 }
 // - a method that adds up all the possible scores
 addPossible(){
-   const possGrade = this.possible
-    possGrade.reduce(currentValue, previousValue  => (currentValue + previousValue))
-    console.log(addPossible())
+    return this.possible.reduce((possGrade, currentGrade) => (possGrade + currentGrade))
 }
 
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
-letterGrade(currentGrade, possGrade){
-    if ((currentGrade / possGrade) * 100 == 100) {
-    document.querySelector("#certGrade").innerHTML = "A+"
-}else if((currentGrade/ possGrade)* 100 <= 95){
-    document.querySelector("#certGrade").innerHTML = "A-"
-}else if((currentGrade/ possGrade)* 100 <= 90){
-    document.querySelector("#certGrade").innerHTML = "B+"
-}else if((currentGrade/ possGrade)* 100 <= 85){
-    document.querySelector("#certGrade").innerHTML = "B-"
-}else if((currentGrade/ possGrade)* 100 <= 80){
-    document.querySelector("#certGrade").innerHTML = "C+"
-}else if((currentGrade/ possGrade)* 100 <= 75){
-    document.querySelector("#certGrade").innerHTML = "C-"
-}else if((currentGrade/ possGrade)* 100 <= 70){
-    document.querySelector("#certGrade").innerHTML = "D+"
-}else if((currentGrade/ possGrade)* 100 <= 65){
-    document.querySelector("#certGrade").innerHTML = "D-"
-}else if((currentGrade/ possGrade)* 100 < 60){
-    document.querySelector("#certGrade").innerHTML = "F"
-    console.log(this.letterGrade)
+letterGrade(){
+    var now = (this.addScores()/this.addPossible()) *100
+    if (now > 90) return "A"
+    if (now > 80) return "B"
+    if (now > 70) return "C"
+    if (now > 60) return "D"
+    else return"F"
+
 }
-}}
+}
