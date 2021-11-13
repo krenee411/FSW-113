aaa47597e5c363098bc5d6e00a31d605a720f6fc
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-11-13 13:04:15 -0600

    capstone added

--- capstone/scripts/bug.js
+++ capstone/scripts/bug.js
@@ -1,28 +1,115 @@
+//import {data} from "./data.js"
 // Create a variable of the right kind and in the right place such that each new bug that is added can increment that number
+var data;
 
 class Bug {
-    constructor() {
+    constructor(reportBy, sysDiv, subDiv, bugDiv) {
+        this.reportBy = reportBy;
+        this.sys = sysDiv;
+        this.sub = subDiv;
+        this.bug = bugDiv;
+        //**you need to have gets and sets here 
         // This constructor should be set up to accept the four user-input values from index.html: 
-        // reportedBy, system, subSystem, and bugDesc
+        
+    }
+    getReport(){
+        return this.reportBy
+    }
+    getSys(){
+        return this.sysDiv
+    }
+    getSub(){
+        return this.subDiv
+    }
+    getBug(){
+        return this.bugDiv
     }
 
-    addBug() {
+    addBug(info) {
+        
+        var wrapper = document.querySelector("#listWrapper") 
+        var newDiv = document.createElement("div")
+        newDiv.setAttribute("id", "newDiv")
+        wrapper.appendChild(newDiv)
+        newDiv.append(info)  
+
+        var deleteBtn = document.createElement("BUTTON")
+        deleteBtn.setAttribute("id", "deleteBtn")
+        let x = ("&#10006")
+        deleteBtn.innerHTML = x 
+        deleteBtn.addEventListener("click", this.deleteBug)
+        newDiv.appendChild(deleteBtn)
+        
+        var resBtn = document.createElement("BUTTON")
+        resBtn.setAttribute("id", "resBtn")
+        let c =  ("&#10004")
+        resBtn.innerHTML = c
+        resBtn.addEventListener("click", this.resolveBug)
+        newDiv.appendChild(resBtn)
+           
+        
         // Create a div element that displays the bug information input by the user within the "listWrapper" DOM element. 
         // It should also contain buttons whose onClick events will call the deleteBug() and resolveBug() methods (see below). 
 
     }
 
-    deleteBug() {
+    deleteBug(e) {
+       // console.log(e.target.parentNode)
+        e.target.parentNode.remove()
+            
+        }
+        
+
+        //**get item by the id with a querySelector()
+        //**and remove it with the removeChild() method
         // Create code that will remove the appropriate bug from the DOM. 
         // You may need to Google how to remove an element from the DOM.
-    }
+    
+        resolveBug(e) {
+            //console.log(e.target.parentNode)
+            e.target.parentNode.style.backgroundColor = "black"
+            e.target.parentNode.style.color = "white"
 
-    resolveBug() {
-        // Create code that changes the appropriate bug report to a darker color
-    }
+            
+               //i need to add somthing here so when clicked it will change the color with .style.color = "black"
+           
+           //**document.quesySelector("bugId_"+somthing.getId()).style.color = "black"
+           // Create code that changes the appropriate bug report to a darker color
+       }
+
+  
 }
+async function getSystems(parentID, DOM) {
+    let wait;
+    const getFetch = await fetch("./data.json")
+    wait = await getFetch.json()
+      data = wait
+}
+getSystems();
 
 function reportBug() {
+    
+    let report = document.querySelector('#reportedBy').value;
+    let sys = document.querySelector('#system').value; //how do i get my on change to work in this function
+    let sub = document.querySelector('#subSystem').value;
+    let bug = document.querySelector('#bugDesc').value;
+    var newBug = new Bug(report,data.systems[parseInt(sys)-1].sysName,data.systems[parseInt(sub)-1].sysName,bug)
+    //console.log(data)
+    // console.log(sys)
+    // console.log(parseInt(sys)-1)
+    // console.log(data.systems[1])
+    // console.log(data.systems[parseInt(sys)-1].sysName)
+
+    newBug.addBug(("Reported by: " + newBug.reportBy + "\n "
+     + " System:" + newBug.sys +"/ "+ newBug.sub +  "\n"
+     + "Message:" +  newBug.bug + "\n"))
+    return false;
+   // document.querySelector("#submitBtn").addEventListener("click", () =>{
+      // var myDiv =  document.querySelector("#newDiv")
+       //myDiv.innerHTML = ("Reported by: " + report + " System: " + sys +"/ "+ sub + "Message: " + bug)
+      //  console.log(myDiv)
+   // })
     // Create code that instantiates the Bug class with the data input by the 
     // user in the index.html form. Then call the method to add the new bug report.
 }
+
--- capstone/scripts/data.js
+++ capstone/scripts/data.js
@@ -1,26 +1,89 @@
 // Call the "getSystems()" function in such a way that when the page loads, the "system" select element displays the three sytems 
 // whose parentID is zero.
+//**systemDiv-- is where all the parentID 0 goes- or will be populated
+var firstDrop = document.querySelector("#system") //first option dropdown
+var secondDrop = document.querySelector("#subSystem")// second option dropdown
+export var data;
+var first;
+var second;
 
-function getSystems() {
-    // This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
-    // Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
-    // After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
+
+//const data = require("./../data.json")
+//import data from "./data.json"
+//console.log(data)
+
+async function getSystems(parentID, DOM) {
+    let wait;
+    const getFetch = await fetch("./data.json")
+    wait = await getFetch.json()
+      data = wait
+    
+     var filterData = data.systems.filter((object) =>{ // this is going to add the options to the dropdown box
+        if(object.parentID == parentID){
+            return object
+        }
+    
+    })
+
+    populateDD(filterData, DOM)
+
+    // >This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
+    // >Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
+    // >After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
     // those items from the JSON that have the matching parentID. That promise should then call a function that passes two parameters, 
-    // this new array and the DOM element to the "populateDD" function (below).
+    // >this new array and the DOM element to the "populateDD" function (below).
 
     // Note that although a number being passed as a parameter into a function may look like a numeral, it may be a string value 
     // instead, and may need to be converted to an integer. 
+    //make a string a number with praseInt()
 }
+getSystems(0,firstDrop);
 
-function populateDD() {
+firstDrop.addEventListener("change", function(){
+    let first = firstDrop.selectedOptions[0].text
+    console.log(first)
+})
+
+secondDrop.addEventListener("change", function(){
+    let second = secondDrop.selectedOptions[0].text
+})
+
+
+function populateDD(filterAry, DOM) {
+    DOM.add(new Option ("Select an Item",""))
+    filterAry.forEach(obj => {
+        var newOption = new Option(obj.sysName, obj.sysID)
+            console.log(newOption)
+            DOM.add(newOption)
+    })
+    //**does it only get the matching parentID since we are calling on the firstdrop? 
+    
+
     // This function receives the array and DOM element from the "getSystems()" function (above). This function should fill the 
     // appropriate DOM element with options from which the user can select. However, since that DOM element has an "onChange" event, this
     // function first needs to add an option that says "Select an Item". Then use a looping mechanism (week one) to create the rest of  
     // the select element's options using the sysName and sysID from the passed in array (see week five).
 }
 
-document.querySelector('#system').addEventListener(
+document.querySelector('#system').addEventListener("change", (e) => { //this e if for event
+    secondDrop.innerHTML = ""
+    getSystems(parseInt(e.target.value),secondDrop);
+
+    
+    /*
+    for(var i = 0; i<data.systems.length; i++){
+        var obj= data.systems[i]
+    if(obj.parentID === parseInt(e.target.value)){ 
+        var newOption = new Option(obj.sysName, obj.sysID)
+            console.log(obj)
+            secondDrop.add(newOption)//**does it only get the matching parentID since we are calling on the secondDrop?   
+        //console.log(data.systems[i].parentID)
+    }
+} */
+
+}
     // This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
     // to the "getSystems()" function along with the "subSystem" DOM element so that the second drop-down list is populated with the 
     // appropriate sub-systems from the data.json file.
 )
+
